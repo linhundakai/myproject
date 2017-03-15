2d5f68c744ab37d880b15837bade628d417cd83f
linhong.li <linhong.li@ncqlinhong-li.msxf.com>
2017-03-15 18:13:49 +0800

    test

---
+++ spring-cloud-demo/service0/src/main/java/cn/demo/service0/controller/Service0Controller.java
@@ -0,0 +1,27 @@
+package cn.demo.service0.controller;
+
+import org.apache.commons.lang.time.FastDateFormat;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * @Author: Leo
+ * @Blog: http://blog.csdn.net/lc0817
+ * @CreateTime: 2017/1/19 12:13
+ * @Description:
+ */
+@RestController
+public class Service0Controller {
+  @Value("${test.value}")
+  String test;
+  
+    @GetMapping("test")
+    String test() {
+      System.err.println(test);
+        return FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss.sss").format(new Date());
+    }
+
+}
---
+++ spring-cloud-demo/service1/src/main/java/cn/demo/service1/controller/Service1Controller.java
@@ -0,0 +1,30 @@
+package cn.demo.service1.controller;
+
+import cn.demo.service1.client.Service0Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Leo
+ * @Blog: http://blog.csdn.net/lc0817
+ * @CreateTime: 2017/1/19 14:21
+ * @Description:
+ */
+@RestController
+public class Service1Controller {
+
+    @Autowired
+    Service0Client service0Client;
+
+    @Value("{test.value}")
+    String test;
+    
+    @GetMapping("/test")
+    public String test() {
+      System.err.println(test);
+        return service0Client.test();
+    }
+
+}
